557f2f89cd9fef817bc8d2ce968639572562b381
LeftAutomated <bryantle2021@gmail.com>
2022-07-21 01:25:17 -0500

    feat(api): create gin router

--- main.go
+++ main.go
@@ -1,12 +1,28 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
 
+func getPing(c *gin.Context) {
+    c.JSON(http.StatusOK, gin.H{
+        "message": "pong",
+    })
+}
+
 func main() {
-	fmt.Println("Hello World")
+    router := gin.Default()
+    
+    proxies := []string{"127.0.0.1"}
+    router.SetTrustedProxies(proxies)
+
+    router.GET("/ping", getPing)
+
+    host := "localhost"
+    port := "8080"
+    url := host + ":" + port
+
+    router.Run(url)
 }
