41cf3d89fad743334a3f27c8bb66ab768b314e4f
ramirezfernando <ramirez.fernando2003@gmail.com>
2023-11-19 02:16:43 -0600

    feat(/users/points): implement logic to interact with supabase

--- api/users/points/index.go
+++ api/users/points/index.go
@@ -1,18 +1,50 @@
 package points
 
 import (
-  "net/http"
+	"encoding/json"
+	"net/http"
+	"github.com/codecoogs/gogo/wrappers/http"
+	"github.com/codecoogs/gogo/wrappers/supabase"
 )
 
+type UserPoints struct {
+	Points int `json:"points"`
+}
+
 func Handler(w http.ResponseWriter, r *http.Request) {
-	id := r.URL.Query().Get("id")
+	crw := &codecoogshttp.ResponseWriter{W: w}
+  	crw.SetCors(r.Host)
+
+	client, err := codecoogssupabase.CreateClient()
+	if err != nil {
+		crw.SendJSONResponse(http.StatusInternalServerError, map[string]string{"message": "Failed to create Supabase client: " + err.Error()})
+		return
+	}
+
+  	id := r.URL.Query().Get("id")
   
 	switch r.Method {
 	case "GET":
-		w.Write([]byte("GET request to points of user with id: " + id))
+		var userPoints []UserPoints
+		if _, err := client.From("User").Select("points", "exact", false).Eq("id", id).ExecuteTo(&userPoints); err != nil {
+			crw.SendJSONResponse(http.StatusInternalServerError, map[string]string{"message": "Failed to get users points: " + err.Error()})
+			return
+		}
+		crw.SendJSONResponse(http.StatusOK, map[string]interface{}{"data": userPoints})
 	case "PATCH":
-		w.Write([]byte("PATCH request to points of user with id: " + id))
+		var updatedUserPoints UserPoints
+		if err := json.NewDecoder(r.Body).Decode(&updatedUserPoints); err != nil {
+			crw.SendJSONResponse(http.StatusBadRequest, map[string]string{"message": "Invalid request body: " + err.Error()})
+			return
+		}
+		// TODO: Perform validation checks on updatedUserPoints data
+		
+		if _, _, err := client.From("User").Update(updatedUserPoints, "", "exact").Eq("id", id).Execute(); err != nil {
+			crw.SendJSONResponse(http.StatusInternalServerError, map[string]string{"message": "Failed to update user points: " + err.Error()})
+			return
+		}
+		crw.SendJSONResponse(http.StatusOK, map[string]string{"message": "Successfully updated user points with id: " + id})
 	default:
-		w.Write([]byte("Invalid request type"))
+		crw.SendJSONResponse(http.StatusMethodNotAllowed, map[string]string{"message": "Method not allowed for this resource"})
 	}
-  }
+}
