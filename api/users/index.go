24998f1b8bef44a1588c697fa5a3502f8e656d66
ramirezfernando <ramirez.fernando2003@gmail.com>
2023-11-19 00:28:29 -0600

    feat(/users): implement logic to interact with supabase

--- api/users/index.go
+++ api/users/index.go
@@ -1,20 +1,68 @@
 package users
 
 import (
-  "net/http"
+	"encoding/json"
+	"net/http"
+	"github.com/codecoogs/gogo/wrappers/http"
+	"github.com/codecoogs/gogo/wrappers/supabase"
+	"github.com/google/uuid"
 )
 
+type User struct {
+  ID uuid.UUID `json:"id"`
+  Role string `json:"role"`
+  FirstName string `json:"first_name"`
+  LastName string `json:"last_name"`
+  Email string `json:"email"`
+  Phone string `json:"phone"`
+  Password string `json:"password"`
+  Classification string `json:"classification"`
+  ExpectedGraduation string `json:"expected_graduation"`
+  Discord *string `json:"discord"`
+  Team *uuid.UUID `json:"team"`
+  Points int `json:"points"`
+}
+
 func Handler(w http.ResponseWriter, r *http.Request) {
+  crw := &codecoogshttp.ResponseWriter{W: w}
+  crw.SetCors(r.Host)
+
+  client, err := codecoogssupabase.CreateClient()
+  if err != nil {
+    crw.SendJSONResponse(http.StatusInternalServerError, map[string]string{"message": "Failed to create Supabase client: " + err.Error()})
+    return
+  }
+
   id := r.URL.Query().Get("id")
 
   switch r.Method {
   case "GET":
-	  w.Write([]byte("GET request to user with id: " + id))
+    var user []User
+    if _, err := client.From("User").Select("*", "exact", false).Eq("id", id).ExecuteTo(&user); err != nil {
+      crw.SendJSONResponse(http.StatusInternalServerError, map[string]string{"message": "Failed to get users: " + err.Error()})
+      return
+    }
+    crw.SendJSONResponse(http.StatusOK, map[string]interface{}{"data": user})
   case "PUT":
-	  w.Write([]byte("PUT request to user with id: " + id))
+    var updatedUser User
+    if err := json.NewDecoder(r.Body).Decode(&updatedUser); err != nil {
+      crw.SendJSONResponse(http.StatusBadRequest, map[string]string{"message": "Invalid request body: " + err.Error()})
+      return
+    }
+    // TODO: Perform validation checks on updatedUser data
+    
+    if _, _, err := client.From("User").Update(updatedUser, "", "exact").Eq("id", id).Execute(); err != nil {
+        crw.SendJSONResponse(http.StatusInternalServerError, map[string]string{"message": "Failed to update user: " + err.Error()})
+        return
+    }
+    crw.SendJSONResponse(http.StatusOK, map[string]string{"message": "Successfully updated user with id: " + id})
   case "DELETE":
-	  w.Write([]byte("DELETE request to user with id: " + id))
+    if _, _, err := client.From("User").Delete("", "exact").Eq("id", id).Execute(); err != nil {
+      crw.SendJSONResponse(http.StatusInternalServerError, map[string]string{"message": "Failed to delete users: " + err.Error()})
+      return
+    }
+    crw.SendJSONResponse(http.StatusOK, map[string]string{"message": "Successfully deleted user with id: " + id})
   default:
-	  w.Write([]byte("Invalid request type"))
+    crw.SendJSONResponse(http.StatusMethodNotAllowed, map[string]string{"message": "Method not allowed for this resource"})
   }
 }
--- go.mod
+++ go.mod
@@ -2,6 +2,9 @@ module github.com/codecoogs/gogo
 
 go 1.18
 
-require github.com/supabase-community/supabase-go v0.0.1
+require (
+	github.com/google/uuid v1.4.0
+	github.com/supabase-community/supabase-go v0.0.1
+)
 
 require github.com/supabase/postgrest-go v0.0.7 // indirect
