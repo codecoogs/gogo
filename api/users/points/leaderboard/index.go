8854c7d3dd921910c22fc9d6c43fd26eeb7ff5a4
ramirezfernando <ramirez.fernando2003@gmail.com>
2023-12-08 20:45:35 -0600

    feat: add get leaderboard endpoint

---
+++ api/users/points/leaderboard/index.go
@@ -0,0 +1,107 @@
+package leaderboard
+
+import (
+	"net/http"
+	"strconv"
+
+	"github.com/codecoogs/gogo/wrappers/http"
+	"github.com/codecoogs/gogo/wrappers/supabase"
+	"github.com/google/uuid"
+	"github.com/supabase-community/supabase-go"
+)
+
+type LeaderboardUser struct {
+	LeaderboardId uuid.UUID `json:"leaderboard_id"`
+	UserId        uuid.UUID `json:"user_id"`
+	Rank          int       `json:"rank"`
+}
+
+type Response struct {
+	Success bool              `json:"success"`
+	Data    []LeaderboardUser `json:"data,omitempty"`
+	Error   *ErrorDetails     `json:"error,omitempty"`
+}
+
+type ErrorDetails struct {
+	Message string `json:"message"`
+}
+
+func Handler(w http.ResponseWriter, r *http.Request) {
+	crw := &codecoogshttp.ResponseWriter{W: w}
+	crw.SetCors(r.Host)
+
+	client, err := codecoogssupabase.CreateClient()
+	if err != nil {
+		crw.SendJSONResponse(http.StatusInternalServerError, Response{
+			Success: false,
+			Error: &ErrorDetails{
+				Message: "Failed to create Supabase client: " + err.Error(),
+			},
+		})
+		return
+	}
+
+	top := r.URL.Query().Get("top")
+	if top == "" {
+		crw.SendJSONResponse(http.StatusBadRequest, Response{
+			Success: false,
+			Error: &ErrorDetails{
+				Message: "Provide 'top' as a parameter to retrive the 'top' users",
+			},
+		})
+		return
+	}
+
+	switch r.Method {
+	case "GET":
+		leaderboard, err := getLeaderboard(client, top)
+		if err != nil {
+			crw.SendJSONResponse(http.StatusInternalServerError, Response{
+				Success: false,
+				Error: &ErrorDetails{
+					Message: err.Error(),
+				},
+			})
+			return
+		}
+
+		if leaderboard == nil {
+			crw.SendJSONResponse(http.StatusBadRequest, Response{
+				Success: false,
+				Error: &ErrorDetails{
+					Message: "Not enough users exist",
+				},
+			})
+			return
+		}
+
+		crw.SendJSONResponse(http.StatusOK, Response{
+			Success: true,
+			Data:    leaderboard,
+		})
+	default:
+		crw.SendJSONResponse(http.StatusMethodNotAllowed, Response{
+			Success: false,
+			Error: &ErrorDetails{
+				Message: "Method not allowed for this resource",
+			},
+		})
+	}
+}
+
+func getLeaderboard(client *supabase.Client, top string) ([]LeaderboardUser, error) {
+	var leaderboard []LeaderboardUser
+	count, err := strconv.Atoi(top)
+	if err != nil {
+		return nil, err
+	}
+
+	if _, err := client.From("Leaderboard_User").Select("*", "exact", false).Limit(count, "").ExecuteTo(&leaderboard); err != nil {
+		return nil, err
+	}
+	if len(leaderboard) == 0 {
+		return nil, nil
+	}
+
+	return leaderboard, nil
+}
