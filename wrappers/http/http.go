9fb8b73f8d99677d3b3318b15fb90419e43042cc
Bryant Le <bryantle2021@gmail.com>
2024-03-19 12:30:19 -0500

    fix: cors origin access

--- wrappers/http/http.go
+++ wrappers/http/http.go
@@ -18,6 +18,8 @@ type ResponseWriter struct {
 }
 
 func (crw *ResponseWriter) SetCors(origin string) {
+  // TODO: handle development and production client side origins
+  /*
 	if strings.Contains(origin, "www.codecoogs.com") {
 		crw.W.Header().Set(ControlOriginHeader, "https://www.codecoogs.com")
 	}
@@ -25,7 +27,8 @@ func (crw *ResponseWriter) SetCors(origin string) {
 	if strings.Contains(origin, "127.0.0.1") {
 		crw.W.Header().Set(ControlOriginHeader, "http://localhost:3001")
 	}
-
+  */
+	crw.W.Header().Set(ControlOriginHeader, "*")
 	crw.W.Header().Set("Access-Control-Allow-Methods", "*")
 }
 
